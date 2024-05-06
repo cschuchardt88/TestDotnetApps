d2323056480c0728fed5851c8c7884a7090fbfd9
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-05-06 06:56:21 -0400

    fixes

--- src/TestSHA3/Program.cs
+++ src/TestSHA3/Program.cs
@@ -1,14 +1,13 @@
-﻿using Neo.Cryptography;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
-ECDsa ecdsa = ECDsaOpenSsl.Create(ECCurve.CreateFromFriendlyName("secP256k1"));
-ECParameters s = ecdsa.ExportParameters(true);
-byte[] bytes = Encoding.UTF8.GetBytes("Hello");
-var signature = ecdsa.SignData(bytes, HashAlgorithmName.SHA3_256);
-var result = Crypto.VerifySignature(bytes, signature, [0x04, .. s.Q.X, .. s.Q.Y], Neo.Cryptography.ECC.ECCurve.Secp256k1);
+//ECDsa ecdsa = ECDsaOpenSsl.Create(ECCurve.CreateFromFriendlyName("secP256k1"));
+//ECParameters s = ecdsa.ExportParameters(true);
+//byte[] bytes = Encoding.UTF8.GetBytes("Hello");
+//var signature = ecdsa.SignData(bytes, HashAlgorithmName.SHA3_256);
+//var result = Crypto.VerifySignature(bytes, signature, [0x04, .. s.Q.X, .. s.Q.Y], Neo.Cryptography.ECC.ECCurve.Secp256k1);
 
-Console.WriteLine("SHA3_256: {0}", Convert.ToHexString(signature));
+//Console.WriteLine("SHA3_256: {0}", Convert.ToHexString(signature));
 
 var shake256 = new Shake256();
 
