5c8559df72bc113208971f9133cc5fec794bc51f
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-05-06 06:53:04 -0400

    fixes

--- src/TestSHA3/Program.cs
+++ src/TestSHA3/Program.cs
@@ -5,11 +5,11 @@ using System.Text;
 ECDsa ecdsa = ECDsaOpenSsl.Create(ECCurve.CreateFromFriendlyName("secP256k1"));
 ECParameters s = ecdsa.ExportParameters(true);
 byte[] bytes = Encoding.UTF8.GetBytes("Hello");
-var signature = ecdsa.SignData(bytes, HashAlgorithmName.SHA256);
+var signature = ecdsa.SignData(bytes, HashAlgorithmName.SHA3_256);
 var result = Crypto.VerifySignature(bytes, signature, [0x04, .. s.Q.X, .. s.Q.Y], Neo.Cryptography.ECC.ECCurve.Secp256k1);
 
-Console.WriteLine(Convert.ToHexString(signature));
+Console.WriteLine("SHA3_256: {0}", Convert.ToHexString(signature));
 
 var shake256 = new Shake256();
 
-Console.WriteLine(Convert.ToHexString(Shake256.HashData(Encoding.UTF8.GetBytes("Hello"), 256)));
+Console.WriteLine("Shake256: {0}", Convert.ToHexString(Shake256.HashData(Encoding.UTF8.GetBytes("Hello"), 256)));
